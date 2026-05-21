081ede441656ab7b133f85ce966d2fc4a6645a3e
Shoy09 <jhoel.dioses@tecsup.edu.pe>
2026-05-21 04:01:47 -0500

    3

--- src/routes/api/MaterialRoutes.js
+++ src/routes/api/MaterialRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const materialController = require('../../controllers/materialController');
+const materialController = require('../../controllers/materialcontroller.js');
 const verificarToken = require('../../middleware/auth');
 
 router.get('/', verificarToken, materialController.getAllMateriales);
