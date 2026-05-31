7574f6bb2e9a024e90dfb3d0b6c9685000db5d43
Shoy09 <jhoel.dioses@tecsup.edu.pe>
2026-05-31 01:40:11 -0500

    prueba

--- src/routes/api/EmpresaRoutes.js
+++ src/routes/api/EmpresaRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const empresaController = require('../../controllers/empresaController');
+const empresaController = require('../../controllers/EmpresaController');
 const verificarToken = require('../../middleware/auth');
 
 router.get('/', verificarToken, empresaController.getAll);
